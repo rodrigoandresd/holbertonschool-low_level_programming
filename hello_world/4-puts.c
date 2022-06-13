fd273ebd5b6f857d41eaa8eb0b393f24320afb0d
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-13 15:27:20 -0500

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  pusts("Programming is like building a multilingual puzzle\n");
-  return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
