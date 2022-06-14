d91214705320d43b31170f92d33d9aa46fdfe65f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-14 17:54:14 -0500

    alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-char ch;
+	char ch, e, q;
+
+	e = 'e'
+	q = 'q'
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	if (ch != 'e' && ch != 'q')
-putchar(ch);
+	putchar(ch);
 }
 	putchar('\n');
 return (0);
