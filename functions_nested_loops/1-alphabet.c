0ce62daddae04c877c912fee9a52e586b9dfe27c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-15 14:32:30 -0500

    alp

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * write alphabet
  * Return: Always 0 (Success)
  */
-int print_alphabet(void)
+void print_alphabet(void)
+
 {
 	char c;
-	
-	for (c = 'a' ; c <= 'z' ; c++)	
+
+	for (c = 'a' ; c <= 'z' ; c++) 
 	_putchar(c);
 	_putchar('\n');
-	return (0);
 }
 
