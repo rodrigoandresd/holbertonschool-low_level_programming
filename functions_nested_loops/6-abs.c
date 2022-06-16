695473fbba7602d7f317823137ec6b5dca8d508c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-15 21:04:20 -0500

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Check
+ * @i: An input character
+ * Description: function uses _abs
+ * integral o
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
