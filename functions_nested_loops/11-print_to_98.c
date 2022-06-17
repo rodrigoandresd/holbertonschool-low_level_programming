18e1424c577900af3657033e084045aeab12c775
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-17 18:43:59 -0500

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,21 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n < 98)
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
-				}
+		}
+	}
+	else if (n > 98)
+	{
 		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
-		printf("%d, ", n);
 	}
+	printf("98\n");
 }
