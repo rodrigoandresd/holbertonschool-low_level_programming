2b630d88e562c45ca7a9c09949726419487b7b68
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-20 11:32:55 -0500

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * mul - check the code.
+ * @a: Variable digit
+ * @b: variable dig
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+return (prod);
+}
+
