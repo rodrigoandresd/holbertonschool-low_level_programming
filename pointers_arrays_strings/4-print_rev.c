cf2151133b2941ef14ea27cd563b34c38ac8d338
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-21 18:18:09 -0500

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev -  prints a string in rev
+ * @s: phrase
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+int letter = 0;
+	for (letter = 0 ; s[letter] != '\0' ; letter++)
+	{
+	}
+		for (letter = letter - 1 ; letter >= 0 ; letter--)
+		{
+			putchar(s[letter]);
+		}
+		putchar('\n');
+	
+}
+
