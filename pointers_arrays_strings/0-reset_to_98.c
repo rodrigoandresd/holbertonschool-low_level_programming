9a33e6cb18daa954fd36854237678f2258c61b84
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-21 13:49:07 -0500

    reset to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
- /**
-  * reset_to_98 -funtion takes a pointer to an int 
-  * @n: variable puntero
-  * return: (0)
-  */
+/**
+ * reset_to_98 -funtion takes a pointer to an int
+ * @n: variable puntero
+ * return: (0)
+ */
 
 void reset_to_98(int *n)
 {
