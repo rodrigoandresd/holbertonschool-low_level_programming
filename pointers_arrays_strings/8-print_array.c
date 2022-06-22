66f054c4ccb82cf2c6f575ea01aa500685ae38e7
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-22 12:33:38 -0500

    print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints a number second half
+ * @n:  is the number of elements of the array to be printed
+ * @a: pointer
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int imput;
+
+	for (imput = 0 ; imput < 5 ; imput++)
+	{
+	if (imput != (n - 1))
+		printf("%d, ", a[imput]);
+	else
+		printf("%d", a[imput]);
+	}
+printf("\n");
+}
