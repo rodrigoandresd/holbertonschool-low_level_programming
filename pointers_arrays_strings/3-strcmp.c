9266b6ac04227f5147d1a830cbaabc181fea0cde
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-27 18:57:44 -0500

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
-int o;
+	int i;
 
-for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
-o = s2[i] - s1[i];
-return (o);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
 
