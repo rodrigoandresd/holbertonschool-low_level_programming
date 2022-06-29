e1236e4b7ad8fc9408a254dbb428a3701d5e2bf3
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-29 14:26:00 -0500

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strstr - function that locates a substring.
+ * @haystack: pointer initial segment
+ * @needle: pointer bytes
+ * Return: i
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+		if (haystack[i] == needle[j])
+		needle += j;
+		return (needle + j);
+		}
+	}
+	return (0);
+}
+
