552a82dabcaa7fb2e4167c3f8c2f2bc1ecbd4e88
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-29 16:30:18 -0500

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -16,11 +16,12 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] != needle[j])
-			break;
+			if (haystack[i + j] == needle[j])
+			i++;
+			else
+				break;
 		}
-		if (needle[j] != '\0')
-			return (needle + i);
+		return (needle + i);
 	}
 	return (0);
 }
