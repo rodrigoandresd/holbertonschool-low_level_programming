c303a6d113b92f0f4a9e93578e3ff987bbba15b0
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-06-28 21:59:41 -0500

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *n;
-	n = NULL;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 	if (s[i] == c)
 	return (s + i);
 	}
-return (n);
+return (NULL);
 }
 
