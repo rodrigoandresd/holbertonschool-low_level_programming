da59c9961103f8197083c0650174c8c41a160387
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-06 18:49:40 -0500

    third by betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,7 +6,7 @@
  * *str_concat - function that concatenates two strings
  * @s1: first string
  * @s2: second string
- * Return:
+ * Return: strin concatenated
  */
 
 char *str_concat(char *s1, char *s2)
