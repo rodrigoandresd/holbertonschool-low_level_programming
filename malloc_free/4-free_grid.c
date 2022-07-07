85abf41439e48903335e8d793f40e95d303d1358
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-07 13:50:45 -0500

    3 commit 4 errors betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -9,14 +9,14 @@
  * Return: nothing
  */
 
-void free_grid(int **grid, int height) 
+void free_grid(int **grid, int height)
 {
 	int i;
 
-    for(i = 0 ; i < height ; i++)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+	for (i = 0 ; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+free(grid);
 }
 
