127fc4ddbb74ba8bf78c31d6ef9c3efc1d5a0a26
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-08 17:07:47 -0500

    first t0 malloc2

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using mall-c
+ * @b: status
+ * Return: funtion
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (p);
+}
