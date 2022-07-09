d65c6de0bc978db5e00ca9ea016ddbc536d29d7b
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-08 23:33:40 -0500

    calloc first commit

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: number of the memory space
+ * @size: size
+ * Return: funtion
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *spc;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+
+	spc = malloc(nmemb * size);
+	if (spc == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < (nmemb * size) ; i++)
+	spc[i] = 0;
+	return (spc);
+}
