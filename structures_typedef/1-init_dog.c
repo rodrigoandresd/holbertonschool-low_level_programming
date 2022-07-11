4a40b255076b0c92eab941db873c3793a57d4642
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-11 16:56:23 -0500

    fourth one

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,9 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d;
+	struct dog *d = my_dog;
 
-	d = &my_dog;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
