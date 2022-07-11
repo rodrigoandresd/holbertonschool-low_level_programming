eba2db299033c6f5e6c94977e1e670dc4987a1d1
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-11 15:05:21 -0500

    first commit

---
+++ structures_typedef/README.md
@@ -0,0 +1,10 @@
+Structures, typedef  in C
+
+Learning proyect
+
+objectives 
+
+General
+What are structures, when, why and how to use them
+How to use typedef
+
---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - description dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} my_dog;
+
+#endif
