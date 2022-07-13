6ae66d79db217bc58a967f59b92e99f207af8735
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-12 19:02:01 -0500

    first name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name:name 
+ * @f: funtion pointed
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
---
+++ function_pointers/README.md
@@ -0,0 +1,5 @@
+Function pointer in c
+
+What are function pointers and how to use them
+What does a function pointer exactly hold
+Where does a function pointer point to in the virtual memory
