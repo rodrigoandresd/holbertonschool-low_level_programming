c98f45e4d789af526ad58ea7898603c51a112ae4
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-13 16:06:21 -0500

    first calc

---
+++ function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - runs calc
+ * @argc: counter
+ * @argv: values
+ * Return: 0 sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int calc;
+	int (*operation)(int, int);
+
+	if (argc != 4) /* valid number of arguments */
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	calc = operation(num1, num2);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (num2 == '\0')
+	{
+	printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: operation
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - substract
+ * @a: integer
+ * @b: integer
+ * Return: operation
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: integer
+ * @b: integer
+ * Return: operation
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divition
+ * @a: integer
+ * @b: integer
+ * Return: operation
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: integer
+ * @b: integer
+ * Return: operation
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
