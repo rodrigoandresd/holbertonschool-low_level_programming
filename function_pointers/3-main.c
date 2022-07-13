4807ce02f826c8714adc16e5b1ddc5991644dd4b
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-13 16:49:49 -0500

    2 calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -27,16 +27,17 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	operation = get_op_func(argv[2]);
 	calc = operation(num1, num2);
+	sim = *argv[2];
 
 	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	sim = *argv[2];
-	if ((sim == '/' || sim == '%') && *argv[3] == '0')
+	
+	if ((sim == '/' || sim == '%') && num2 == '0')
 	{
-	printf("Error\n");
+		printf("Error\n");
 		exit(100);
 	}
 	printf("%d\n", calc);
