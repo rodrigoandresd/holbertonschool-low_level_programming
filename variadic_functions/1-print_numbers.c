c70558f4bddeb2b061c38d766b5b95cbb5e6007c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-15 13:28:42 -0500

    first commit print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,9 +1,26 @@
-include "variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
- *
- *
- *
+ * print_numbers- function that prints numbers
+ * @n: number of arguments
+ * @separator: pointer a separator
+ * Return: sum of all its arguments
  */
 
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
 
+	unsigned int i;
+
+	va_start(ap, n);
+
+		for (i = 0 ; i < n ; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if ((i + 1) != n && separator != NULL)
+				printf("%s", separator);
+		}
+	printf("\n");
+	va_end(ap);
+}
