9ae2ad49c7c619dec19a0a751e27c578b41b2961
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-14 20:20:55 -0500

    templates

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef _NAME_OF_FILE_H
+#define _NAME_OF_FILE_H
+
+/* importing */
+
+/* types and structures */
+
+/* macros valores constantes */
+
+/* prototypes */
+
+/* macros functions */
+
+#endif /* _NAME_OF_FILE_H */
