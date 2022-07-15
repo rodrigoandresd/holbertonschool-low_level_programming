3ea1d079f5a54440f7a9631d6991fc6f76ae5b43
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-15 14:01:13 -0500

    first commit string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,9 +1,32 @@
-include "variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
- *
- *
- *
+ * print_strings - function that prints strings
+ * @n: number of arguments
+ * @separator: pointer a separator
+ * Return: nothing
  */
 
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *string;
 
+	va_start(ap, n);
+
+		for (i = 0 ; i < n ; i++)
+		{
+		string = va_arg(ap, char *);
+			if (ap == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			printf("%s", string);
+			if ((i + 1) != n && separator != NULL)
+				printf("%s", separator);
+		}
+	printf("\n");
+	va_end(ap);
+}
