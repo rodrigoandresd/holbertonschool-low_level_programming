04986472a74aa95e452947ebec345c7330596c63
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-28 00:23:20 -0500

    add file task 7 get_nodeint_at_index

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 1;
 	listint_t *temp;
-
+	
+	if (head == NULL)
+		return (NULL);
 	temp = head;
 	while (i <= index)
 	{
