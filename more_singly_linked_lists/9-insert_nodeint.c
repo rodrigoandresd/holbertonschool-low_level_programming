3f646f3644aecaa1f55472d705550b95279abd89
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-07-29 00:19:10 -0500

    add file task 9 insert_nodeint

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
+	newnode->n = n;
 
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 	temp = *head;
 
 	while (i < idx - 1)
@@ -30,7 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (temp == NULL)
 			return (NULL);
 	}
-	newnode->n = n;
 	newnode->next = temp->next;
 	temp->next = newnode;
 	return (newnode);
