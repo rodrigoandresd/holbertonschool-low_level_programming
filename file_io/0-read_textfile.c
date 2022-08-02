7810a35d82782788014f22097c5318a63991e877
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-01 19:48:38 -0500

    create file task 0 read texfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to SO
+ * @letters: is the number of letters it should read and print
+ * @filename: name of the file to be read
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	char *buffer;
+	ssize_t letter_read;
+	ssize_t letter_write;
+	ssize_t number_letter_rw;
+
+	if (filename == NULL)
+	return (0);
+
+	fp = open(filename,  O_RDONLY);
+
+	if (fp == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		free(buffer);
+		close(fp);
+		return (0);
+	}
+
+	letter_read = read(fp, buffer, letters);
+	letter_write = write(STDOUT_FILENO, buffer, letter_read);
+
+	if (letter_write == -1 || letter_write != letter_read)
+	{
+		free(buffer);
+		close(fp);
+		return (0);
+	}
+
+	number_letter_rw = letter_write;
+	close(fp);
+	free(buffer);
+	return (number_letter_rw);
+}
+
+
