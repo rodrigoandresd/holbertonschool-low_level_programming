18106dc210607c1c5614090bea30379fce09d538
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-02 16:24:12 -0500

    create file task 3 cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+  * free_file_from - freefile from in case error
+  * @file_from: name of file from
+  * @buffer: buffer to free
+  * Return: void
+  */
+
+void case_error_file_from(const char *file_from, char *buffer)
+{
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+
+/**
+  * case_error_file_to - error procedure when file to cant be created
+  * @file_to: name of file to
+  * @buffer: buffer to free
+  * Return: void
+  */
+void case_error_file_to(char *file_to, char *buffer)
+{
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
+
+/**
+  * case_error_close - error procedure when issue with closing
+  * @fd: integer where file is opened
+  * @buffer: buffer to free
+  * Return: void
+  */
+void case_error_close(int fd, char *buffer)
+{
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
  * copy_text_file - function that copy text from file to other file
  * @file_from: the name of the file to create
  * @file_to: file new copy
@@ -11,47 +51,36 @@ int copy_text_file(const char *file_from, char *file_to)
 	int f_d_from, f_d_to, readed, writed;
 	char *buffer;
 
-	if (file_from == NULL)
-	return (98);
-
-	f_d_from = open(file_from, O_RDONLY);
-	if (f_d_from == -1)
-		return (98);
-
-	f_d_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (f_d_to == -1)
-		return (99);
-
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
-		close(f_d_from);
-		return (98);
+		return (0);
 	}
+
+	f_d_from = open(file_from, O_RDONLY);
+	if (f_d_from == -1)
+		case_error_file_from(file_from, buffer);
+
+	f_d_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f_d_to == -1)
+		case_error_file_to(file_to, buffer);
+
 	readed = 1;
 	while (readed)
 	{
 		readed = read(f_d_from, buffer, 1024);
 		if (readed == -1)
-		{
-			close(f_d_from);
-			close(f_d_to);
-			free(buffer);
-			return (98);
-		}
+			case_error_file_from(file_from, buffer);
 		writed = write(f_d_to, buffer, readed);
 		if (writed == -1)
-		{
-			close(f_d_from && f_d_to);
-			free(buffer);
-			return (99);
-		}
+			case_error_file_to(file_to, buffer);
 	}
 	close(f_d_from);
 	close(f_d_to);
 	free(buffer);
 	return (0);
 }
+
 /**
  * main - check the code
  * @ac: number of arguments
@@ -60,29 +89,12 @@ int copy_text_file(const char *file_from, char *file_to)
  */
 int main(int ac, char **av)
 {
-	int copied;
-
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	return (0);
-	copied = copy_text_file(av[1], av[2]);
-	if (copied == 98)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	if (copied == 99)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	if (copied != 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copied);
-		exit(100);
-	}
+	copy_text_file(av[1], av[2]);
 	return (0);
 }
+
