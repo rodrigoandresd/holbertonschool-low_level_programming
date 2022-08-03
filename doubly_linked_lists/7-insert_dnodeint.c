c628f1dff34cb72bbfd7c8a6990b9031097d21a4
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-03 10:45:11 -0500

    create file task 7 insert_dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *temp = NULL;
 	dlistint_t *newnode = NULL;
-	
+
 	if (*h == NULL)
 		return (NULL);
 
