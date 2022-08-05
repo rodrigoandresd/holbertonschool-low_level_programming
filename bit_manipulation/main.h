e7b5234e928c2e84372ee7197ef9ec8dbee9b6d7
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-04 21:33:55 -0500

    add putchar main_h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_H
 
 /* importing */
+#include <stdio.h>
+
+/* prototipes */
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
