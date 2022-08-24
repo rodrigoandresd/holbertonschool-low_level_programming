fe38eac779eefe66c68e9876ecfba2929ef37d9f
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-24 10:39:42 -0500

    add advance 100 print_conb3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,24 +6,24 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = '0'; i <= '8'; i++)
-    {
-        for (j = '0'; j <= '9'; j++)
-        {
-            if (j > i)
-            {
-                putchar(i);
-                putchar(j);
-                if (i != '8')
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
