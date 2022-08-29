cc330c7c07b3ca377dd8cf5c7318490372863989
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-29 15:52:33 -0500

    correction error memory allocate

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash_table->array == NULL)
 	{
-		free (new_hash_table);
+		free(new_hash_table);
 		return (NULL);
 	}
 	new_hash_table->size = size;
