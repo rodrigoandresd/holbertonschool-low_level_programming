405a684d7ac75e4eb623a2c8101eea0e7ee03627
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-30 11:27:33 -0500

    add task 5 hash_table_print

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (separator != 0)
 				printf(", ");
 			printf("'%s': '%s'", temp->key, temp->value);
-			separator = 0;
+			separator = 1;
 			temp = temp->next;
 		}
 	}
