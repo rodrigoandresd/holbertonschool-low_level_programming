7e15372e5a088b0542e1e637f2f2640cb4c3118d
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-08-29 22:40:06 -0500

    correction allocation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,10 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_table = NULL;
 	unsigned long int i = 0;
 
-	if (!size)
-		return (NULL);
-
-	new_hash_table = malloc(sizeof(hash_node_t));
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
 
