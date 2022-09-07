260622dc0087db8cd76aa51fbf0c8b86b3f2147d
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-09-07 12:43:18 -0500

    add task 0

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+/* importing */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* types and structures */
+
+/* macros valores constantes */
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+
+/* macros functions */
+
+#endif /* _SEARCH_ALGOS_H */
